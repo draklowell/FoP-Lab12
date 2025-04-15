3ed1baef7b801ec280bb28ca2ada2232b932f205
Andriy Kryvyi <kryvyi.andriy@gmail.com>
2025-04-15 09:14:26 +0300

    Add solution for "Delete Node in a BST" problem

---
+++ delete_node_in_bst/problem.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findSmallest(self, root: TreeNode) -> TreeNode:
+        while root.left is not None:
+            return root.left
+
+        return root
+
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if root is None:
+            return None
+
+        if root.val > key:
+            root.left = self.deleteNode(root.left, key)
+            return root
+
+        if root.val < key:
+            root.right = self.deleteNode(root.right, key)
+            return root
+
+        if root.right is None:
+            return root.left
+
+        if root.left is None:
+            return root.right
+
+        smallest = self.findSmallest(root.right)
+        smallest.left = root.left
+        return root.right
